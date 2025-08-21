a7e10b5b15d17f3faa174b85e7f88b1ba54a8fca
shreeshacharya <143174861+shreeshacharya@users.noreply.github.com>
2025-08-21 21:52:02 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,18 @@
+// Fibonacci Series in Java
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 10; // number of terms to print
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            // update values
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}

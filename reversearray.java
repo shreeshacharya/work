6b8df8f5a9f75521696099419b0bfbca8a1c5cc4
shreesha <143174861+shreeshacharya@users.noreply.github.com>
2025-08-28 19:20:15 +0530

    Add files via upload

---
+++ reversearray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class reversearray {
+
+    public static void reverse(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int temp=arr[end];
+            arr[end]=arr[start];
+            arr[start]=temp;
+            start++;
+            end--;
+
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}

15b25a12021b32d1a498ca5ffd77150d61c5e8c1
shreesha <143174861+shreeshacharya@users.noreply.github.com>
2025-08-31 21:27:34 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+    public static void main(String[] args) {
+        // int[] arr=new int[10];
+        // arr[1]=11;
+        // arr[3]=22;
+        // System.out.println(arr[1]);
+        // System.out.println(arr[4]);
+
+        int arr[]={1,2,3,4,5};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+    
+}

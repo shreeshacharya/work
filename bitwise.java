f181a645633814c486617455132050490f296365
shreesha <143174861+shreeshacharya@users.noreply.github.com>
2025-09-05 21:00:48 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,8 @@
+public class bitwise {
+    public static void main(String[] args) {
+        System.out.println(10<<2);
+        System.out.println(10>>2);
+        System.out.println(10&2);
+    }
+    
+}

c98720832e7a949f06601fef51f6447abad95e9b
shreesha <143174861+shreeshacharya@users.noreply.github.com>
2025-09-19 23:10:46 +0530

    Add files via upload

--- fibonacci.java
+++ fibonacci.java
@@ -1,21 +1,22 @@
-
 import java.util.Scanner;
 
-public class fibonacci{
+public class fibonacci {
     public static void main(String[] args) {
-        int first=0;
-        int second=1;
+        int first = 0;
+        int second = 1;
         int next;
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter a no");
-        int n=sc.nextInt();
-        System.out.println("so the fibonacci sequence for the "+n+"term are:");
-        for(int i=0;i<n;i++){
-            System.out.println(first+"");
-            first=second;
-            next=first+second;
-            second=next;
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of terms:");
+        int n = sc.nextInt();
+
+        System.out.println("Fibonacci sequence for " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(first);
+            next = first + second;
+            first = second;
+            second = next;
         }
+
     }
 }

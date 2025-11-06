988a2be13f8345e6ea53a9b27e051665d730cfba
shreesha <143174861+shreeshacharya@users.noreply.github.com>
2025-11-06 21:01:26 +0530

    Add files via upload

---
+++ oddeven_ternary.java
@@ -0,0 +1,11 @@
+public class oddeven_ternary {
+    public static void even(int a){
+        String res=a%2==0?"even":"odd";
+        System.out.println(res);
+
+    }
+    public static void main(String[] args) {
+        even(10);
+    }
+    
+}
